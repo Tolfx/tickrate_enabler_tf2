ab73f973543e6ebf6abf067b4a94756825f6aea3
Tolfx <matteus@tolfx.dev>
2023-08-08 15:16:13 +0200

    try

--- tickrate_enabler.cpp
+++ tickrate_enabler.cpp
@@ -1,35 +1,35 @@
 #include "eiface.h"
-#include "sourcehook/sourcehook_impl.h"
+#include "metamod-source/core/sourcehook/sourcehook_impl.h"
 class TickrateEnabler : public IServerPluginCallbacks
 {
 public:
-	TickrateEnabler() { };
-	~TickrateEnabler() { };
+	TickrateEnabler(){};
+	~TickrateEnabler(){};
 
 	// IServerPluginCallbacks methods
 	virtual bool Load(CreateInterfaceFn interfaceFactory, CreateInterfaceFn gameServerFactory);
 	virtual void Unload(void);
-	virtual void Pause(void) { };
-	virtual void UnPause(void) { };
+	virtual void Pause(void){};
+	virtual void UnPause(void){};
 	virtual const char *GetPluginDescription(void);
-	virtual void LevelInit(char const *pMapName) { };
-	virtual void ServerActivate(edict_t *pEdictList, int edictCount, int clientMax) { };
-	virtual void GameFrame(bool simulating) { };
-	virtual void LevelShutdown(void) { };
-	virtual void ClientActive(edict_t *pEntity) { };
-	virtual void ClientFullyConnect(edict_t *pEntity) { };
-	virtual void ClientDisconnect(edict_t *pEntity) { };
-	virtual void ClientPutInServer(edict_t *pEntity, char const *playername) { };
-	virtual void SetCommandClient(int index) { };
-	virtual void ClientSettingsChanged(edict_t *pEdict) { };
+	virtual void LevelInit(char const *pMapName){};
+	virtual void ServerActivate(edict_t *pEdictList, int edictCount, int clientMax){};
+	virtual void GameFrame(bool simulating){};
+	virtual void LevelShutdown(void){};
+	virtual void ClientActive(edict_t *pEntity){};
+	virtual void ClientFullyConnect(edict_t *pEntity){};
+	virtual void ClientDisconnect(edict_t *pEntity){};
+	virtual void ClientPutInServer(edict_t *pEntity, char const *playername){};
+	virtual void SetCommandClient(int index){};
+	virtual void ClientSettingsChanged(edict_t *pEdict){};
 	virtual PLUGIN_RESULT ClientConnect(bool *bAllowConnect, edict_t *pEntity, const char *pszName, const char *pszAddress, char *reject, int maxrejectlen) { return PLUGIN_CONTINUE; };
 	virtual PLUGIN_RESULT ClientCommand(edict_t *pEntity, const CCommand &args) { return PLUGIN_CONTINUE; };
 	virtual PLUGIN_RESULT NetworkIDValidated(const char *pszUserName, const char *pszNetworkID) { return PLUGIN_CONTINUE; };
-	virtual void OnQueryCvarValueFinished(QueryCvarCookie_t iCookie, edict_t *pPlayerEntity, EQueryCvarValueStatus eStatus, const char *pCvarName, const char *pCvarValue) { };
+	virtual void OnQueryCvarValueFinished(QueryCvarCookie_t iCookie, edict_t *pPlayerEntity, EQueryCvarValueStatus eStatus, const char *pCvarName, const char *pCvarValue){};
 
 	// added with version 3 of the interface.
-	virtual void OnEdictAllocated(edict_t *edict) { };
-	virtual void OnEdictFreed(const edict_t *edict) { };
+	virtual void OnEdictAllocated(edict_t *edict){};
+	virtual void OnEdictFreed(const edict_t *edict){};
 
 	// added with version 4 of the interface.
 	virtual bool BNetworkCryptKeyCheckRequired(uint32 unFromIP, uint16 usFromPort, uint32 unAccountIdProvidedByClient, bool bClientWantsToUseCryptKey) { return false; };
